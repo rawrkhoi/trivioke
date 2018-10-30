d1a6f03b48e813abca47b6680ae647fabe1953ab
Dario Elbadry <elbadry.d@gmail.com>
2018-10-29 21:58:57 -0500

    added db save and post request handler

--- db/mysql.js
+++ db/mysql.js
@@ -7,4 +7,24 @@ const connection = mysql.createConnection({
   database: 'trivioke',
 });
 
-connection.connect();
+connection.connect((err) => {
+  if (err) {
+    console.log('db connection error');
+  } else {
+    console.log('Connected');
+  }
+});
+
+// this should only happen once;
+const save = (data) => {
+  const q = `insert into songs(artist, title, uri) values (${data.artist}, ${data.title}, ${data.url}, )`;
+  connection.query(q, (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('songs saved to db');
+    }
+  });
+};
+
+module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const axios = require('axios');
+const key = require('../config.js');
+const db = require('../db/mysql.js');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/../dist/index.html`));
+
+app.post('/songs', (req, res) => {
+  const url = 'https://www.googleapis.com/youtube/v3/search';
+  const params = {
+    part: 'snippet',
+    type: 'video',
+    key: key.youtube,
+    channelId: 'UCwTRjvjVge51X-ILJ4i22ew',
+    maxResults: 50,
+  };
+  axios.get(url, params)
+    .then((data) => {
+      data.forEach((song) => {
+        db.save(song);
+      });
+      res.send(200);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
