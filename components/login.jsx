a810810e0edcddaa04e023fe5e177bb6a2a44e99
Josh Nguyen <khoixkt@gmail.com>
2018-11-03 18:14:15 -0500

    Redirect from login working

--- components/login.jsx
+++ components/login.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable no-restricted-globals */
 import React from 'react';
 import axios from 'axios';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Redirect } from 'react-router';
 
 class Login extends React.Component {
@@ -17,26 +16,6 @@ class Login extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // routing() {
-  //   <Router>
-  //     <div>
-  //       <Route exact path="/login" component={Login} />
-  //       {/* <Route exact path="/trivia" component={Load} /> */}
-  //       <Route exact path="/video" component={VideoPlayer} />
-  //     </div>
-  //   </Router>;
-  // }
-
-  // updateRoute() {
-  //   return (
-  //     <Router>
-  //       <Route exact path="/login" render={() => this.state.redirect ? (
-  //         <Redirect to="/trivia" /> ) : (
-  //         <Redirect to="/login" /> )}/>
-  //     </Router>
-  //   );
-  // }
-
   handleChange() {
     this.setState({
       [event.target.name]: event.target.value,
@@ -47,10 +26,9 @@ class Login extends React.Component {
     const loginInfo = this.state;
     axios({ method: 'get', url: 'http://localhost:8080/login', params: loginInfo })
       .then((response) => {
-        // console.log('got back');
         this.setState({ redirect: true });
       })
-      .catch((err) => console.log(err))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -63,11 +41,11 @@ class Login extends React.Component {
             <label>
                   Username:
               <input type="text" name="name" onChange={this.handleChange} />
-              </label>
-                <label>
+            </label>
+            <label>
                   Password:
-                  <input type="text" name="pw" onChange={this.handleChange} />
-                </label>
+              <input type="text" name="pw" onChange={this.handleChange} />
+            </label>
             <input type="submit" value="Submit" onClick={this.handleSubmit} />
           </div>
         </div>
