5fa82988e8689532a29199e0cd32109df26534f1
Dario Elbadry <elbadry.d@gmail.com>
2018-11-04 16:04:33 -0600

    added server error msg

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.configure(() => {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
 app.use(express.static(`${__dirname}/../build`));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
